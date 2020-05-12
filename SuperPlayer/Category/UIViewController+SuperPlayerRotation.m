f558adc9280938d2aa82073a6b0d1503139314e1
zhouzheng <1173035171@qq.com>
2020-05-12 17:19:15 +0800

    修改支持的屏幕方向

--- SuperPlayer/Category/UIViewController+SuperPlayerRotation.m
+++ SuperPlayer/Category/UIViewController+SuperPlayerRotation.m
@@ -8,7 +8,7 @@
 
 // 支持哪些屏幕方向
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskPortrait;
+    return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
 // 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
